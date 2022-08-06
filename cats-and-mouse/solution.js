07d047dc377911a84ae4a9b79d5642004111bddd
unknown <arongrhyz@gmail.com>
2022-08-07 03:33:34 +0800

    added solution code

---
+++ cats-and-mouse/solution.js
@@ -0,0 +1,9 @@
+function catAndMouse(x, y, z) {
+  if (Math.abs(z - x) > Math.abs(z - y)) {
+    return "Cat B";
+  } else if (Math.abs(z - x) < Math.abs(z - y)) {
+    return "Cat A";
+  } else {
+    return "Mouse C";
+  }
+}

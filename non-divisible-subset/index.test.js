6251dcdd6280ecdf23230c72d4750e6bcfab063b
unknown <arongrhyz@gmail.com>
2022-09-28 23:16:07 +0800

    added test cases for the problem non divisible subset

---
+++ non-divisible-subset/index.test.js
@@ -0,0 +1,6 @@
+describe("Hackerrank Problem: Non Divisible Subset", () => {
+  it.todo("Should return 3 with inputs: 4, [1, 7, 2, 4]");
+  it.todo(
+    "Should return 11 with inputs: 7, [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436]"
+  );
+});
